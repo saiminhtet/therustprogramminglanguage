de2ad7c5ed68e29fcabfcce646922a1cbd9e65c3
saiminhtet <minhtet_sai@MIS200100025.NECSAPSIN.COM>
2022-09-13 18:00:57 +0800

    added hecto text-editor

---
+++ hecto/src/main.rs
@@ -0,0 +1,81 @@
+// use std::io::{self, stdout, Read};
+// use std::io::{self, stdout};
+// use termion::event::Key;
+// use termion::input::TermRead;
+// use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+
+use editor:: Editor;
+
+// fn to_ctrl_byte(c: char) -> u8 {
+//     let byte = c as u8;
+//     byte & 0b0001_1111
+// }
+
+// fn die(e: std::io::Error) {
+//     panic!("{}", e);
+// }
+
+
+
+fn main() {
+    let editor = Editor::default();
+    editor.run();
+    // println!("Hello, world!");
+    // let _stdout = stdout().into_raw_mode().unwrap();
+
+    // for b in io::stdin().bytes() {
+    // let c = b.unwrap() as char;
+    // println!("{}", c);
+    // let b = b.unwrap();
+    // let c = b as char;
+    // if c.is_control() {
+    //     println!("{:?} \r", b);
+    //     println!("{:#b}", b);
+    // } else {
+    //     println!("{:?} ({})\r", b, c);
+    // }
+
+    // if b == to_ctrl_byte('q') {
+    //     break;
+    // }
+
+    // if c == 'q' {
+    //    break;
+    // }
+
+    // match b {
+    //     Ok(b) => {
+    //         let c = b as char;
+    //         if c.is_control() {
+    //             println!("{:?} \r", b);
+    //         } else {
+    //             println!("{:?} ({})\r", b, c);
+    //         }
+
+    //         if b == to_ctrl_byte('q') {
+    //             break;
+    //         }
+    //     }
+    //     Err(err) => die(err),
+    // }
+    // }
+
+    // for key in io::stdin().keys {
+    //     match key {
+    //         Ok(key) => match key {
+    //             Key::Char(c) => {
+    //                 if c.is_control() {
+    //                     println!("{:?}\r", c as u8);
+    //                 } else {
+    //                     println!("{:?} ({})\r", c as u8, c);
+    //                 }
+    //             }
+    //             Key::Ctrl('q') => break,
+    //             _ => println!("{:?}\r",key),
+    //         },
+    //         Err(err) => die(err),
+    //     }
+    // }
+}
